f57fc673d41dadfc6139a42df1d094ea75656090
Joshua Redmond <Github@JoshuaRedmond.co.uk>
2016-06-09 22:38:29 +0100

    Working button!

--- electron/main.js
+++ electron/main.js
@@ -1,5 +1,5 @@
 const electron = require('electron')
-const usb = require('electron-usb')
+// const usb = require('electron-usb')
 const menubar = require('menubar')
 
 // Module to control application life.
@@ -12,21 +12,21 @@ const mb = menubar()
 mb.on('ready', function ready () {
   console.log('app is ready')
   //console.log(usb.getDeviceList());
-  var teensy = usb.findByIds(5824,1155);
-  teensy.open();
- // console.log(teensy.interfaces)
+ //  var teensy = usb.findByIds(5824,1155);
+ //  teensy.open();
+ // // console.log(teensy.interfaces)
 
- // console.log(teensy.interfaces[0].claim())
+ // // console.log(teensy.interfaces[0].claim())
 
- // console.log(teensy.interfaces[0].endpoint(130))
+ // // console.log(teensy.interfaces[0].endpoint(130))
 
 
-  console.log("-----");
-   console.log(teensy.interfaces[1].claim())
+ //  console.log("-----");
+ //   console.log(teensy.interfaces[1].claim())
 
-  var outEndpoint = teensy.interfaces[1].endpoint(3)
-  console.log(teensy.interfaces[1].endpoint(3))
-  outEndpoint.transfer('1')
+ //  var outEndpoint = teensy.interfaces[1].endpoint(3)
+ //  console.log(teensy.interfaces[1].endpoint(3))
+ //  outEndpoint.transfer('1')
   // your app code here
 })
 
--- electron/renderer.js
+++ electron/renderer.js
@@ -1,3 +1,18 @@
+const usb = require('electron-usb')
+
+var count = true
+
+var teensy = usb.findByIds(5824,1155);
+  	teensy.open();
+   teensy.interfaces[1].claim()
+  	var outEndpoint = teensy.interfaces[1].endpoint(3)
+
 document.getElementById('flagbutton').addEventListener('click', function() {
-    alert('hello!');
+    //alert('hello!');
+    
+  //	console.log(teensy.interfaces[1].endpoint(3))
+
+  	 count ? outEndpoint.transfer('1') : outEndpoint.transfer('0')
+	 count = !count
+
 });
