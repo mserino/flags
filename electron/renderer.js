c8074964f6555c5adff004e216070d073e5433a6
Fernando Macias Pereznieto <f@pereznieto.name>
2016-06-10 10:01:58 +0100

    UI
    Makes a pretty nice initial GUI :)

--- electron/renderer.js
+++ electron/renderer.js
@@ -4,25 +4,25 @@ const annyang = require('annyang')
 var count = true
 
 try {
-  var teensy = usb.findByIds(5824,1155);
-  teensy.open();
-  teensy.interfaces[1].claim()
-  var outEndpoint = teensy.interfaces[1].endpoint(3)
+	var teensy = usb.findByIds(5824,1155);
+	teensy.open();
+	teensy.interfaces[1].claim()
+	var outEndpoint = teensy.interfaces[1].endpoint(3)
 } catch(err) {
-  alert('Oops. Something went wrong. Please try reconnecting the device and restarting the application. Idiot.')
+	alert('Oops. Something went wrong. Please try reconnecting the device and restarting the application. Idiot.')
 }
 
 function sendToggleRequest() {
-  count ? outEndpoint.transfer('1') : outEndpoint.transfer('0')
+	count ? outEndpoint.transfer('1') : outEndpoint.transfer('0')
 	count = !count
 }
 
 function sendStartRequest() {
-  count = outEndpoint.transfer('1')
+	count = outEndpoint.transfer('1')
 }
 
 function sendStopRequest() {
-  count = outEndpoint.transfer('0')
+	count = outEndpoint.transfer('0')
 }
 
 // Turn on debug messages
@@ -30,9 +30,9 @@ annyang.debug()
 
 // Define sample command
 var commands = {
-  'flags start': sendStartRequest,
-  'flags stop': sendStopRequest,
-  'flags change': sendToggleRequest
+	'flags start': sendStartRequest,
+	'flags stop': sendStopRequest,
+	'flags change': sendToggleRequest
 }
 
 // Add our commands to annyang
@@ -41,4 +41,5 @@ annyang.addCommands(commands)
 // Start listening. You can call this here, or attach this call to an event, button, etc.
 annyang.start()
 
-document.getElementById('flagbutton').addEventListener('click', sendToggleRequest)
+document.getElementById('flagButtonOne').addEventListener('click', sendStartRequest)
+document.getElementById('flagButtonTwo').addEventListener('click', sendStopRequest)
