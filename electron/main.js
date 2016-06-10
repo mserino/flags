d97000dbc926e4660ddaa43e8d9d7fbef752a245
Huw Martin <Home@MacBook-Pro.local>
2016-06-10 08:56:21 +0100

    Adds voice recognition!
    Adds voice recognition with annoying; various other cleanup.

--- electron/main.js
+++ electron/main.js
@@ -1,33 +1,14 @@
 const electron = require('electron')
-// const usb = require('electron-usb')
 const menubar = require('menubar')
 
 // Module to control application life.
 const app = electron.app
+
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
 const mb = menubar()
 
-mb.on('ready', function ready () {
+mb.on('ready', function ready() {
   console.log('app is ready')
-  //console.log(usb.getDeviceList());
- //  var teensy = usb.findByIds(5824,1155);
- //  teensy.open();
- // // console.log(teensy.interfaces)
-
- // // console.log(teensy.interfaces[0].claim())
-
- // // console.log(teensy.interfaces[0].endpoint(130))
-
-
- //  console.log("-----");
- //   console.log(teensy.interfaces[1].claim())
-
- //  var outEndpoint = teensy.interfaces[1].endpoint(3)
- //  console.log(teensy.interfaces[1].endpoint(3))
- //  outEndpoint.transfer('1')
-  // your app code here
 })
-
-
--- electron/renderer.js
+++ electron/renderer.js
@@ -1,18 +1,34 @@
 const usb = require('electron-usb')
+const annyang = require('annyang')
 
 var count = true
 
-var teensy = usb.findByIds(5824,1155);
-  	teensy.open();
-   teensy.interfaces[1].claim()
-  	var outEndpoint = teensy.interfaces[1].endpoint(3)
+try {
+  var teensy = usb.findByIds(5824,1155);
+  teensy.open();
+  teensy.interfaces[1].claim()
+  var outEndpoint = teensy.interfaces[1].endpoint(3)
+} catch(err) {
+  alert('Oops. Something went wrong. Please try reconnecting the device and restarting the application. Idiot.')
+}
 
-document.getElementById('flagbutton').addEventListener('click', function() {
-    //alert('hello!');
-    
-  //	console.log(teensy.interfaces[1].endpoint(3))
+function sendRequest() {
+  count ? outEndpoint.transfer('1') : outEndpoint.transfer('0')
+	count = !count
+}
 
-  	 count ? outEndpoint.transfer('1') : outEndpoint.transfer('0')
-	 count = !count
+// Turn on debug messages
+annyang.debug()
 
-});
+// Define sample command
+var commands = {
+  'hello': sendRequest
+}
+
+// Add our commands to annyang
+annyang.addCommands(commands)
+
+// Start listening. You can call this here, or attach this call to an event, button, etc.
+annyang.start()
+
+document.getElementById('flagbutton').addEventListener('click', sendRequest)
