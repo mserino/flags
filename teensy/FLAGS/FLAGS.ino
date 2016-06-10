fc7a5c5b6f5012daa2497ceab81a4c2b7da7b6e2
Joshua Redmond <Github@JoshuaRedmond.co.uk>
2016-06-10 12:49:19 +0100

    Updates servo functionality with light sensor

--- teensy/FLAGS/FLAGS.ino
+++ teensy/FLAGS/FLAGS.ino
@@ -1,10 +1,23 @@
-// Simple LED blink
+#include <Servo.h>
+//
+Servo myservo;
+int pos = 90; 
+int lightLevel;
+bool servoRunning = false;
 
 const int led = LED_BUILTIN;
+const int reflectiveLED = 17;
+
+
+//int pos = 0;
 
 void setup() {
   pinMode(led, OUTPUT);
+  pinMode(reflectiveLED, OUTPUT);
+  digitalWrite(reflectiveLED, HIGH);
   Serial.begin(38400);
+  myservo.attach(10);
+  myservo.write(pos);
 
 }
 
@@ -15,12 +28,43 @@ void loop() {
     if(incomingByte==48)
     {
       digitalWrite(led, LOW);
+      pos = 90;
     }
     if(incomingByte==49)
     {
       digitalWrite(led, HIGH);
+      pos = 0;
+      servoRunning = true;
     }
+    myservo.write(pos);
+
    }
+
+   lightLevel = analogRead(6);
+
+   if(servoRunning && lightLevel < 800)
+   {
+      pos = 90;
+      digitalWrite(led, LOW);
+      servoRunning = false;
+      myservo.write(pos);
+
+    
+   }
+   
+ 
 }
+
+//void loop() {
+//  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+//    // in steps of 1 deg  ree
+//    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+//    delay(1500);                       // waits 15ms for the servo to reach the position
+//  }
+//  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+//    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+//    delay(1500);                       // waits 15ms for the servo to reach the position
+//  }
+//}
 
 
